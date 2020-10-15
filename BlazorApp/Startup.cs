67b94ffcbaba5b62cb68ed9fa3a61345eeb12770
Keith <keithtmccartney@hotmail.com>
2020-10-15 10:03:14 +0100

    A dramatic amount of changes have been implemented into Blazor since this tutorial's creation and there are an abundance of differences. I'm going to look out for a later tutorial to learn more about Blazor, I'll be seeking something in the realm of: https://docs.microsoft.com/en-us/powerapps/developer/common-data-service/webapi/quick-start-blazor-server-app. I think I might need to run some IDE and CLI updates.

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -9,6 +9,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorApp.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BlazorApp
 {
@@ -25,6 +30,31 @@ namespace BlazorApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] {
+                    MediaTypeNames.Application.Octet, // The MimeType of 'application/octet-stream' could be used but the MediaTypeNames' Application alias of 'Octet' is available;
+                    "application/wasm", // The MimeType of 'application/wasm' must be used as the WasmMediaTypeNames' Application alias of 'Wasm' is not available;
+                });
+            });
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
@@ -33,6 +63,10 @@ namespace BlazorApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseResponseCompression();
+
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
